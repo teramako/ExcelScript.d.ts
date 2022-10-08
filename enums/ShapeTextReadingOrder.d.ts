b1f71bf6d0396bcd9e26e4ae2e31644262b98350
teramako <teramako@gmail.com>
2022-10-08 23:29:37 +0900

    enum ShapeTextReadingOrder

---
+++ enums/ShapeTextReadingOrder.d.ts
@@ -0,0 +1,10 @@
+declare namespace ExcelScript {
+	/**
+	 * 図形のテキスト フレームの読み取り順序を指定します。
+	 * @see [ExcelScript.ShapeTextReadingOrder enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.shapetextreadingorder?view=office-scripts)
+	 */
+	export enum ShapeTextReadingOrder {
+		leftToRight,
+		rightToLeft
+	}
+}
