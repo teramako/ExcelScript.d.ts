18086a590d790ded59eedd833e93d3926b212fa0
teramako <teramako@gmail.com>
2022-10-08 20:45:07 +0900

    enum ArrowheadLength

---
+++ enums/ArrowheadLength.d.ts
@@ -0,0 +1,10 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.ArrowheadLength enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.arrowheadlength?view=office-scripts)
+	 */
+	export enum ArrowheadLength {
+		long,
+		medium,
+		short
+	}
+}
