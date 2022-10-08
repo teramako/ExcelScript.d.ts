5a8b5d138b4d98e8b1ab14bf480bb61bf12fadd3
teramako <teramako@gmail.com>
2022-10-08 21:37:42 +0900

    enum ChartGradientStyle

---
+++ enums/ChartGradientStyle.d.ts
@@ -0,0 +1,11 @@
+declare namespace ExcelScript {
+	/**
+	 * グラフ系列のグラデーション スタイルを表します。
+	 * これは地域マップ グラフにのみ適用されます。
+	 * @see [ExcelScript.ChartGradientStyle enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.chartgradientstyle?view=office-scripts)
+	 */
+	export enum ChartGradientStyle {
+		threePhaseColor,
+		twoPhaseColor
+	}
+}
