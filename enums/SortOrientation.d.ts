bf12153d5661a0dedaa43e7d3513daa720d5b76e
teramako <teramako@gmail.com>
2022-10-08 23:41:39 +0900

    enum SortOrientation.

---
+++ enums/SortOrientation.d.ts
@@ -0,0 +1,9 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.SortOrientation enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.sortorientation?view=office-scripts)
+	 */
+	export enum SortOrientation {
+		columns,
+		rows
+	}
+}
