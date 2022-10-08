b334d2293d923b914f96ebcb03e46066da3f9ea2
teramako <teramako@gmail.com>
2022-10-08 21:09:42 +0900

    enum CalculationType

---
+++ enums/CalculationType.d.ts
@@ -0,0 +1,19 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.CalculationType enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.calculationtype?view=office-scripts)
+	 */
+	export enum CalculationType {
+		/**
+		 * これにより、すべてのセルがダーティとしてマークされ、再計算されます。
+		 */
+		full,
+		/**
+		 * これにより、完全な依存関係チェーンが再構築され、すべてのセルがダーティとしてマークされ、再計算されます。
+		 */
+		fullRebuild,
+		/**
+		 * Excel によってダーティのマークが付けられたすべてのセル (揮発性データと変更されたデータの参照先、およびプログラムによりダーティのマークが付けられたセル) を再計算します。
+		 */
+		recalculate
+	}
+}
