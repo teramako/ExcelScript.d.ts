f876820856d5079da45f985fea9352121ce85154
teramako <teramako@gmail.com>
2022-10-08 21:28:23 +0900

    enum ChartDataLabelPosition

---
+++ enums/ChartDataLabelPosition.d.ts
@@ -0,0 +1,19 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.ChartDataLabelPosition enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.chartdatalabelposition?view=office-scripts)
+	 */
+	export enum ChartDataLabelPosition {
+		bestFit,
+		bottom,
+		callout,
+		center,
+		insideBase,
+		insideEnd,
+		invalid,
+		left,
+		none,
+		outsideEnd,
+		right,
+		top
+	}
+}
