cc4d85cf47c96b2a1f46bf6a18f30f286457b610
teramako <teramako@gmail.com>
2022-10-08 20:46:52 +0900

    enum ArrowheadStyle

---
+++ enums/ArrowheadStyle.d.ts
@@ -0,0 +1,13 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.ArrowheadStyle enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.arrowheadstyle?view=office-scripts)
+	 */
+	export enum ArrowheadStyle {
+		diamond,
+		none,
+		open,
+		oval,
+		stealth,
+		triangle
+	}
+}
