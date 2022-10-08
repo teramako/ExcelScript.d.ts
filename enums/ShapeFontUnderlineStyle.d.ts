6987bac9d9d4e637dc751115141cfa62280d2d95
teramako <teramako@gmail.com>
2022-10-08 23:21:19 +0900

    enum ShapeFontUnderlineStyle

---
+++ enums/ShapeFontUnderlineStyle.d.ts
@@ -0,0 +1,25 @@
+declare namespace ExcelScript {
+	/**
+	 * フォントに適用される下線の種類。
+	 * @see [ExcelScript.ShapeFontUnderlineStyle enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.shapefontunderlinestyle?view=office-scripts)
+	 */
+	export enum ShapeFontUnderlineStyle {
+		dash,
+		dashHeavy,
+		dashLong,
+		dashLongHeavy,
+		dotDash,
+		dotDashHeavy,
+		dotDotDash,
+		dotDotDashHeavy,
+		dotted,
+		dottedHeavy,
+		double,
+		heavy,
+		none,
+		single,
+		wavy,
+		wavyDouble,
+		wavyHeavy
+	}
+}
