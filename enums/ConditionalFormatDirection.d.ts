3fe2711a160b921d096af294f4a11feb079bdf8c
teramako <teramako@gmail.com>
2022-10-08 22:08:10 +0900

    enum ConditionalFormatDirection

---
+++ enums/ConditionalFormatDirection.d.ts
@@ -0,0 +1,10 @@
+declare namespace ExcelScript {
+	/**
+	 * 選択範囲の方向を表します。
+	 * @see [ExcelScript.ConditionalFormatDirection enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.conditionalformatdirection?view=office-scripts)
+	 */
+	export enum ConditionalFormatDirection {
+		bottom,
+		top
+	}
+}
