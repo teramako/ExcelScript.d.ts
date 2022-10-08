5b1b38004a56b0c510dc8cfaa8ab98c23d635816
teramako <teramako@gmail.com>
2022-10-08 21:56:06 +0900

    enum ChartTickLabelAlignment

---
+++ enums/ChartTickLabelAlignment.d.ts
@@ -0,0 +1,10 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.ChartTickLabelAlignment enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.chartticklabelalignment?view=office-scripts)
+	 */
+	export enum ChartTickLabelAlignment {
+		center,
+		left,
+		right
+	}
+}
