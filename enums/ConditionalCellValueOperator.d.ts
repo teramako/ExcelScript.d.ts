e7892409fd4442d98cc7bd15bed19398d136fadd
teramako <teramako@gmail.com>
2022-10-08 22:03:31 +0900

    enum ConditionalCellValueOperator

---
+++ enums/ConditionalCellValueOperator.d.ts
@@ -0,0 +1,17 @@
+declare namespace ExcelScript {
+	/**
+	 * テキストの条件付き書式の型の演算子を表します。
+	 * @see [ExcelScript.ConditionalCellValueOperator enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.conditionalcellvalueoperator?view=office-scripts)
+	 */
+	export enum ConditionalCellValueOperator {
+		between,
+		equalTo,
+		greaterThan,
+		greaterThanOrEqual,
+		invalid,
+		lessThan,
+		lessThanOrEqual,
+		notBetween,
+		notEqualTo
+	}
+}
