c6b554fe505293b6d53adaef669857faeace2267
teramako <teramako@gmail.com>
2022-10-08 22:15:11 +0900

    enum ConditionalRangeBorderIndex

---
+++ enums/ConditionalRangeBorderIndex.d.ts
@@ -0,0 +1,11 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.ConditionalRangeBorderIndex enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.conditionalrangeborderindex?view=office-scripts)
+	 */
+	export enum ConditionalRangeBorderIndex {
+		edgeBottom,
+		edgeLeft,
+		edgeRight,
+		edgeTop
+	}
+}
