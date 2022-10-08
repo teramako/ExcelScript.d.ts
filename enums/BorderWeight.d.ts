1c941f8b642feeda61b4ddf3347c5580d2b678e1
teramako <teramako@gmail.com>
2022-10-08 21:05:44 +0900

    enum BorderWeight

---
+++ enums/BorderWeight.d.ts
@@ -0,0 +1,11 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.BorderWeight enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.borderweight?view=office-scripts)
+	 */
+	export enum BorderWeight {
+		hairline,
+		medium,
+		thick,
+		thin
+	}
+}
