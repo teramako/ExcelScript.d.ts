40c72370d234da3652f806ae990a4558d1f2e13d
teramako <teramako@gmail.com>
2022-10-08 21:24:27 +0900

    enum ChartBinType

---
+++ enums/ChartBinType.d.ts
@@ -0,0 +1,11 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.ChartBinType enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.chartbintype?view=office-scripts)
+	 */
+	export enum ChartBinType {
+		auto,
+		binCount,
+		binWidth,
+		category
+	}
+}
