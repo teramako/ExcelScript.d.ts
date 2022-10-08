f2320657f9db67d66dc3589f455908e9368e7a1c
teramako <teramako@gmail.com>
2022-10-08 21:29:21 +0900

    enum ChartDisplayBlanksAs

---
+++ enums/ChartDisplayBlanksAs.d.ts
@@ -0,0 +1,10 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.ChartDisplayBlanksAs enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.chartdisplayblanksas?view=office-scripts)
+	 */
+	export enum ChartDisplayBlanksAs {
+		interplotted,
+		notPlotted,
+		zero
+	}
+}
