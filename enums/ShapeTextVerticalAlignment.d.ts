947c14ca115f631dca13f962874794b1267cdd30
teramako <teramako@gmail.com>
2022-10-08 23:30:42 +0900

    enum ShapeTextVerticalAlignment

---
+++ enums/ShapeTextVerticalAlignment.d.ts
@@ -0,0 +1,13 @@
+declare namespace ExcelScript {
+	/**
+	 * 図形のテキスト フレームの垂直方向の配置を指定します。
+	 * @see [ExcelScript.ShapeTextVerticalAlignment enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.shapetextverticalalignment?view=office-scripts)
+	 */
+	export enum ShapeTextVerticalAlignment {
+		bottom,
+		distributed,
+		justified,
+		middle,
+		top
+	}
+}
