1b0f2b68fd592c4a51572dc54159900273f30d78
teramako <teramako@gmail.com>
2022-10-08 22:56:48 +0900

    enum PivotFilterTopBottomCriterion

---
+++ enums/PivotFilterTopBottomCriterion.d.ts
@@ -0,0 +1,15 @@
+declare namespace ExcelScript {
+	/**
+	 * 上/下の値フィルターの条件を表します。
+	 * @see [ExcelScript.PivotFilterTopBottomCriterion enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.pivotfiltertopbottomcriterion?view=office-scripts)
+	 */
+	export enum PivotFilterTopBottomCriterion {
+		bottomItems,
+		bottomPercent,
+		bottomSum,
+		invalid,
+		topItems,
+		topPercent,
+		topSum
+	}
+}
