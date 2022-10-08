a3d584d97e0dd29919e58a06981f95e8ba101852
teramako <teramako@gmail.com>
2022-10-08 23:24:28 +0900

    enum ShapeScaleFrom

---
+++ enums/ShapeScaleFrom.d.ts
@@ -0,0 +1,10 @@
+declare namespace ExcelScript {
+	/**
+	 * 図形を拡大縮小する場合、図形の位置を保持する部分を指定します。
+	 */
+	export enum ShapeScaleFrom {
+		scaleFromBottomRight,
+		scaleFromMiddle,
+		scaleFromTopLeft
+	}
+}
