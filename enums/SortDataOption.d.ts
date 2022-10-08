11396e510784eecc2392ede8e37336400ae61a2b
teramako <teramako@gmail.com>
2022-10-08 23:38:55 +0900

    enum SortDataOption

---
+++ enums/SortDataOption.d.ts
@@ -0,0 +1,9 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.SortDataOption enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.sortdataoption?view=office-scripts)
+	 */
+	export enum SortDataOption {
+		normal,
+		textAsNumber
+	}
+}
