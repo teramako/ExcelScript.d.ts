ad6fae49ae005ca89820e69a95fb6c5839cf2748
teramako <teramako@gmail.com>
2022-10-08 22:33:17 +0900

    enum FilterDatetimeSpecificity

---
+++ enums/FilterDatetimeSpecificity.d.ts
@@ -0,0 +1,13 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.FilterDatetimeSpecificity enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.filterdatetimespecificity?view=office-scripts)
+	 */
+	export enum FilterDatetimeSpecificity {
+		day,
+		hour,
+		minute,
+		month,
+		second,
+		year
+	}
+}
