134587a70b77b4468ff50a0e49181b04f27753cf
teramako <teramako@gmail.com>
2022-10-08 23:28:26 +0900

    enum ShapeTextOrientation

---
+++ enums/ShapeTextOrientation.d.ts
@@ -0,0 +1,15 @@
+declare namespace ExcelScript {
+	/**
+	 * 図形のテキスト フレームの向きを指定します。
+	 * @see [ExcelScript.ShapeTextOrientation enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.shapetextorientation?view=office-scripts)
+	 */
+	export enum ShapeTextOrientation {
+		eastAsianVertical,
+		horizontal,
+		mongolianVertical,
+		vertical,
+		vertical270,
+		wordArtVertical,
+		wordArtVerticalRTL
+	}
+}
