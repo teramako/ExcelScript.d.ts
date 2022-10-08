16f2f7f52945e7cebed3735b96779d0f8795d452
teramako <teramako@gmail.com>
2022-10-08 21:19:20 +0900

    enum ChartAxisTickLabelPosition

---
+++ enums/ChartAxisTickLabelPosition.d.ts
@@ -0,0 +1,11 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.ChartAxisTickLabelPosition enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.chartaxisticklabelposition?view=office-scripts)
+	 */
+	export enum ChartAxisTickLabelPosition {
+		high,
+		low,
+		nextToAxis,
+		none
+	}
+}
