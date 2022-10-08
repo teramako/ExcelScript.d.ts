7e5160f99f8db129516dba19c6c96fa3fe7869c6
teramako <teramako@gmail.com>
2022-10-08 22:34:06 +0900

    enum FilterOn

---
+++ enums/FilterOn.d.ts
@@ -0,0 +1,17 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.FilterOn enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.filteron?view=office-scripts)
+	 */
+	export enum FilterOn {
+		bottomItems,
+		bottomPercent,
+		cellColor,
+		custom,
+		dynamic,
+		fontColor,
+		icon,
+		topItems,
+		topPercent,
+		values
+	}
+}
