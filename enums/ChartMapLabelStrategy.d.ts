ee973dce98f40978c34e27931b0b36dc30298b76
teramako <teramako@gmail.com>
2022-10-08 21:43:19 +0900

    enum ChartMapLabelStrategy

---
+++ enums/ChartMapLabelStrategy.d.ts
@@ -0,0 +1,12 @@
+declare namespace ExcelScript {
+	/**
+	 * グラフ系列レイアウトの領域レベルを表します。
+	 * これは地域マップ グラフにのみ適用されます。
+	 * @see [ExcelScript.ChartMapLabelStrategy enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.chartmaplabelstrategy?view=office-scripts)
+	 */
+	export enum ChartMapLabelStrategy {
+		bestFit,
+		none,
+		showAll
+	}
+}
