40f80e64fdacc8dc143ed33f50783c12cf56dc89
teramako <teramako@gmail.com>
2022-10-08 21:18:14 +0900

    enum ChartAxisScaleType

---
+++ enums/ChartAxisScaleType.d.ts
@@ -0,0 +1,9 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.ChartAxisScaleType enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.chartaxisscaletype?view=office-scripts)
+	 */
+	export enum ChartAxisScaleType {
+		linear,
+		logarithmic
+	}
+}
