b0a68f3dee5df9ac4a66be19570929c1dd7e6a69
teramako <teramako@gmail.com>
2022-10-08 22:16:02 +0900

    enum ConditionalRangeBorderLineStyle

---
+++ enums/ConditionalRangeBorderLineStyle.d.ts
@@ -0,0 +1,13 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.ConditionalRangeBorderLineStyle enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.conditionalrangeborderlinestyle?view=office-scripts)
+	 */
+	export enum ConditionalRangeBorderLineStyle {
+		continuous,
+		dash,
+		dashDot,
+		dashDotDot,
+		dot,
+		none
+	}
+}
