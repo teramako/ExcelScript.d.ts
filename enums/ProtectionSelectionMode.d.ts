cecd15184421fab07e2435e65568f81d9c762678
teramako <teramako@gmail.com>
2022-10-08 23:04:48 +0900

    enum ProtectionSelectionMode

---
+++ enums/ProtectionSelectionMode.d.ts
@@ -0,0 +1,19 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.ProtectionSelectionMode enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.protectionselectionmode?view=office-scripts)
+	 */
+	export enum ProtectionSelectionMode {
+		/**
+		 * セルの選択は許可されません。
+		 */
+		none,
+		/**
+		 * すべてのセルに対して選択が許可されます。
+		 */
+		normal,
+		/**
+		 * 選択は、ロックされていないセルに対してのみ許可されます。
+		 */
+		unlocked
+	}
+}
