28895caa80ba563e43dc663c87918bde8a1aee67
teramako <teramako@gmail.com>
2022-10-08 22:49:04 +0900

    enum NamedItemType

---
+++ enums/NamedItemType.d.ts
@@ -0,0 +1,14 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.NamedItemType enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.nameditemtype?view=office-scripts)
+	 */
+	export enum NamedItemType {
+		array,
+		boolean,
+		double,
+		error,
+		integer,
+		range,
+		string
+	}
+}
