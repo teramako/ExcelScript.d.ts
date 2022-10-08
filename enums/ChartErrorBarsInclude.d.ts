a725ee7774993dba731053532747d2daa40e9f84
teramako <teramako@gmail.com>
2022-10-08 21:30:38 +0900

    enum ChartErrorBarsInclude

---
+++ enums/ChartErrorBarsInclude.d.ts
@@ -0,0 +1,11 @@
+declare namespace ExcelScript {
+	/**
+	 * 含めるエラー バーの部分を表します。
+	 * @see [ExcelScript.ChartErrorBarsInclude enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.charterrorbarsinclude?view=office-scripts)
+	 */
+	export enum ChartErrorBarsInclude {
+		both,
+		minusValues,
+		plusValues
+	}
+}
