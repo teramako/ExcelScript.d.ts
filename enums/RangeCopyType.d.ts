5e1e6589d16d2da663cb6b9b0b5b7089af49901e
teramako <teramako@gmail.com>
2022-10-08 23:05:37 +0900

    enum RangeCopyType

---
+++ enums/RangeCopyType.d.ts
@@ -0,0 +1,12 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.RangeCopyType enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.rangecopytype?view=office-scripts)
+	 */
+	export enum RangeCopyType {
+		all,
+		formats,
+		formulas,
+		link,
+		values
+	}
+}
