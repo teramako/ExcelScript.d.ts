730a65bc2207809cf31da3f4c6fc9849631abc8f
teramako <teramako@gmail.com>
2022-10-08 22:19:15 +0900

    enum ConditionalTopBottomCriterionType

---
+++ enums/ConditionalTopBottomCriterionType.d.ts
@@ -0,0 +1,13 @@
+declare namespace ExcelScript {
+	/**
+	 * 平均条件付き書式の種類の上/下の条件を表します。
+	 * @see [ExcelScript.ConditionalTopBottomCriterionType enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.conditionaltopbottomcriteriontype?view=office-scripts)
+	 */
+	export enum ConditionalTopBottomCriterionType {
+		bottomItems,
+		bottomPercent,
+		invalid,
+		topItems,
+		topPercent
+	}
+}
