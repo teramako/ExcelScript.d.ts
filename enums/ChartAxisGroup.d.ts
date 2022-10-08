ba9e03386d4a1b392ec58186b490608420c7e676
teramako <teramako@gmail.com>
2022-10-08 21:16:03 +0900

    enum ChartAxisGroup

---
+++ enums/ChartAxisGroup.d.ts
@@ -0,0 +1,9 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.ChartAxisGroup enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.chartaxisgroup?view=office-scripts)
+	 */
+	export enum ChartAxisGroup {
+		primary,
+		secondary
+	}
+}
