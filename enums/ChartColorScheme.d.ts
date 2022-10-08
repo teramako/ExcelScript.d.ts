b79f186fe6df033c3549c6e2f447e6fcefd3cb4a
teramako <teramako@gmail.com>
2022-10-08 21:27:18 +0900

    enum ChartColorScheme

---
+++ enums/ChartColorScheme.d.ts
@@ -0,0 +1,24 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.ChartColorScheme enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.chartcolorscheme?view=office-scripts)
+	 */
+	export enum ChartColorScheme {
+		colorfulPalette1,
+		colorfulPalette2,
+		colorfulPalette3,
+		colorfulPalette4,
+		monochromaticPalette1,
+		monochromaticPalette10,
+		monochromaticPalette11,
+		monochromaticPalette12,
+		monochromaticPalette13,
+		monochromaticPalette2,
+		monochromaticPalette3,
+		monochromaticPalette4,
+		monochromaticPalette5,
+		monochromaticPalette6,
+		monochromaticPalette7,
+		monochromaticPalette8,
+		monochromaticPalette9
+	}
+}
