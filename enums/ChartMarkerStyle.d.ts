36f471a98d0a0be4380cc4fcac6dc86b599991c1
teramako <teramako@gmail.com>
2022-10-08 21:45:39 +0900

    enum ChartMarkerStyle

---
+++ enums/ChartMarkerStyle.d.ts
@@ -0,0 +1,20 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.ChartMarkerStyle enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.chartmarkerstyle?view=office-scripts)
+	 */
+	export enum ChartMarkerStyle {
+		automatic,
+		circle,
+		dash,
+		diamond,
+		dot,
+		invalid,
+		none,
+		picture,
+		plus,
+		square,
+		star,
+		triangle,
+		x
+	}
+}
