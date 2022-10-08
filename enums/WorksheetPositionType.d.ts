bf650636378f514e6a2a5e9d5a06c6aa7578df03
teramako <teramako@gmail.com>
2022-10-08 23:53:37 +0900

    enum WorksheetPositionType

---
+++ enums/WorksheetPositionType.d.ts
@@ -0,0 +1,12 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.WorksheetPositionType enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.worksheetpositiontype?view=office-scripts)
+	 */
+	export enum WorksheetPositionType {
+		after,
+		before,
+		beginning,
+		end,
+		none
+	}
+}
