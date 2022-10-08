8bbf673afc87da827a2da56aa77e3211d20d8a92
teramako <teramako@gmail.com>
2022-10-08 23:03:08 +0900

    enum PrintMarginUnit

---
+++ enums/PrintMarginUnit.d.ts
@@ -0,0 +1,19 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.PrintMarginUnit enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.printmarginunit?view=office-scripts)
+	 */
+	export enum PrintMarginUnit {
+		/**
+		 * ページ余白をセンチメートルで割り当てる。
+		 */
+		centimeters,
+		/**
+		 * ページ余白をインチ単位で割り当てる。
+		 */
+		inches,
+		/**
+		 * ページ余白をポイントで割り当てる。 ポイントは 1/72 インチです。
+		 */
+		points
+	}
+}
