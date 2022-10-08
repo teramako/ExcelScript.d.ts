d60760bbb97ad484305d98081ef934594f455758
teramako <teramako@gmail.com>
2022-10-08 21:20:24 +0900

    enum ChartAxisTickMark

---
+++ enums/ChartAxisTickMark.d.ts
@@ -0,0 +1,11 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.ChartAxisTickMark enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.chartaxistickmark?view=office-scripts)
+	 */
+	export enum ChartAxisTickMark {
+		cross,
+		inside,
+		none,
+		outside
+	}
+}
