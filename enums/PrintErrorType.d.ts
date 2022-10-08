5b90a6501866a3eb42a0c8301455add9a8381145
teramako <teramako@gmail.com>
2022-10-08 23:02:19 +0900

    enum PrintErrorType

---
+++ enums/PrintErrorType.d.ts
@@ -0,0 +1,11 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.PrintErrorType enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.printerrortype?view=office-scripts)
+	 */
+	export enum PrintErrorType {
+		asDisplayed,
+		blank,
+		dash,
+		notAvailable
+	}
+}
