1d6b8c08ca6255d1ff8fc4e8b5be07f706e9e29e
teramako <teramako@gmail.com>
2022-10-08 22:11:09 +0900

    enum ConditionalFormatRuleType

---
+++ enums/ConditionalFormatRuleType.d.ts
@@ -0,0 +1,16 @@
+declare namespace ExcelScript {
+	/**
+	 * 条件付き書式の値の種類を表します。
+	 * @see [ExcelScript.ConditionalFormatRuleType enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.conditionalformatruletype?view=office-scripts)
+	 */
+	export enum ConditionalFormatRuleType {
+		automatic,
+		formula,
+		highestValue,
+		invalid,
+		lowestValue,
+		number,
+		percent,
+		percentile
+	}
+}
