7ab546684c40d5f4a6a10e90dec3c793a5f49881
teramako <teramako@gmail.com>
2022-10-08 21:56:58 +0900

    enum ChartTitlePosition

---
+++ enums/ChartTitlePosition.d.ts
@@ -0,0 +1,13 @@
+declare namespace ExcelScript {
+	/**
+	 * グラフタイトルの位置を表します。
+	 * @see [ExcelScript.ChartTitlePosition enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.charttitleposition?view=office-scripts)
+	 */
+	export enum ChartTitlePosition {
+		automatic,
+		bottom,
+		left,
+		right,
+		top
+	}
+}
