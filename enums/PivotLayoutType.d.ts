6cc735ab4285283b5eb9a728d6642222c87d43a9
teramako <teramako@gmail.com>
2022-10-08 22:59:24 +0900

    enum PivotLayoutType

---
+++ enums/PivotLayoutType.d.ts
@@ -0,0 +1,19 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.PivotLayoutType enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.pivotlayouttype?view=office-scripts)
+	 */
+	export enum PivotLayoutType {
+		/**
+		 * 同じ列の次のフィールドのラベルを持つ水平方向に圧縮されたフォーム。
+		 */
+		compact,
+		/**
+		 * 内部フィールドのアイテムは外部フィールドのアイテムと同じ行に、小計は常に下部に表示されます。
+		 */
+		outline,
+		/**
+		 * 内部フィールドのアイテムは、常に外部フィールドのアイテムを基準に新しい行に表示されます。
+		 */
+		tabular
+	}
+}
