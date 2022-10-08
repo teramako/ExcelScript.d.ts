ca3915d5f77aeec1acb879438196d0be98d31eaf
teramako <teramako@gmail.com>
2022-10-08 23:44:50 +0900

    enum SubtotalLocationType

---
+++ enums/SubtotalLocationType.d.ts
@@ -0,0 +1,19 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.SubtotalLocationType enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.subtotallocationtype?view=office-scripts)
+	 */
+	export enum SubtotalLocationType {
+		/**
+		 * 小計は下部に表示されます。
+		 */
+		atBottom,
+		/**
+		 * 小計は上部に表示されます。
+		 */
+		atTop,
+		/**
+		 * 小計はオフです。
+		 */
+		off
+	}
+}
