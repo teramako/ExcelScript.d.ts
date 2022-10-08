d03b72063301c64126b2fda264db3b9ac6df321c
teramako <teramako@gmail.com>
2022-10-08 22:31:39 +0900

    enum DynamicFilterCriteria

---
+++ enums/DynamicFilterCriteria.d.ts
@@ -0,0 +1,42 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.DynamicFilterCriteria enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.dynamicfiltercriteria?view=office-scripts)
+	 */
+	export enum DynamicFilterCriteria {
+		aboveAverage,
+		allDatesInPeriodApril,
+		allDatesInPeriodAugust,
+		allDatesInPeriodDecember,
+		allDatesInPeriodFebruary,
+		allDatesInPeriodJanuary,
+		allDatesInPeriodJuly,
+		allDatesInPeriodJune,
+		allDatesInPeriodMarch,
+		allDatesInPeriodMay,
+		allDatesInPeriodNovember,
+		allDatesInPeriodOctober,
+		allDatesInPeriodQuarter1,
+		allDatesInPeriodQuarter2,
+		allDatesInPeriodQuarter3,
+		allDatesInPeriodQuarter4,
+		allDatesInPeriodSeptember,
+		belowAverage,
+		lastMonth,
+		lastQuarter,
+		lastWeek,
+		lastYear,
+		nextMonth,
+		nextQuarter,
+		nextWeek,
+		nextYear,
+		thisMonth,
+		thisQuarter,
+		thisWeek,
+		thisYear,
+		today,
+		tomorrow,
+		unknown,
+		yearToDate,
+		yesterday
+	}
+}
