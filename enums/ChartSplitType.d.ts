56216ddb5caa2f336052ce4ac82e86638acd457f
teramako <teramako@gmail.com>
2022-10-08 21:52:52 +0900

    enum ChartSplitType

---
+++ enums/ChartSplitType.d.ts
@@ -0,0 +1,11 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.ChartSplitType enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.chartsplittype?view=office-scripts)
+	 */
+	export enum ChartSplitType {
+		splitByCustomSplit,
+		splitByPercentValue,
+		splitByPosition,
+		splitByValue
+	}
+}
