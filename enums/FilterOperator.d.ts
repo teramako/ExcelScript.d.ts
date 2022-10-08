5c8d7ddc2b57b7f97d2d91bedcc19f104c1c09fd
teramako <teramako@gmail.com>
2022-10-08 22:35:07 +0900

    enum FilterOperator

---
+++ enums/FilterOperator.d.ts
@@ -0,0 +1,9 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.FilterOperator enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.filteroperator?view=office-scripts)
+	 */
+	export enum FilterOperator {
+		and,
+		or
+	}
+}
