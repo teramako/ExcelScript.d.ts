1f3d86262f8eab9a940a1a571dfbf29bcbffaec2
teramako <teramako@gmail.com>
2022-10-08 21:42:12 +0900

    enum ChartMapAreaLevel

---
+++ enums/ChartMapAreaLevel.d.ts
@@ -0,0 +1,17 @@
+declare namespace ExcelScript {
+	/**
+	 * グラフ系列のマッピング レベルを表します。
+	 * これは地域マップ グラフにのみ適用されます。
+	 * @see [ExcelScript.ChartMapAreaLevel enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.chartmaparealevel?view=office-scripts)
+	 */
+	export enum ChartMapAreaLevel {
+		automatic,
+		city,
+		continent,
+		country,
+		county,
+		dataOnly,
+		state,
+		world
+	}
+}
