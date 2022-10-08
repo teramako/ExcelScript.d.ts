c7e9da8fd58df19ac1cfcc42dbdab0e52d2e0067
teramako <teramako@gmail.com>
2022-10-08 23:52:44 +0900

    enum WorkbookLinksRefreshMode

---
+++ enums/WorkbookLinksRefreshMode.d.ts
@@ -0,0 +1,16 @@
+declare namespace ExcelScript {
+	/**
+	 * ブック リンクの更新モードを表します。
+	 * @see [ExcelScript.WorkbookLinksRefreshMode enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.workbooklinksrefreshmode?view=office-scripts)
+	 */
+	export enum WorkbookLinksRefreshMode {
+		/**
+		 * ブックのリンクは、Excel アプリケーションによって決まる一定の間隔で更新されます。
+		 */
+		automatic,
+		/**
+		 * ブックのリンクは手動で更新されます。
+		 */
+		manual
+	}
+}
