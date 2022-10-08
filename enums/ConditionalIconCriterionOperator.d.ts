39ba20c70af84c942dd43d4f3caae432f37386d0
teramako <teramako@gmail.com>
2022-10-08 22:14:23 +0900

    enum ConditionalIconCriterionOperator

---
+++ enums/ConditionalIconCriterionOperator.d.ts
@@ -0,0 +1,11 @@
+declare namespace ExcelScript {
+	/**
+	 * 各アイコン条件の演算子を表します。
+	 * @see [ExcelScript.ConditionalIconCriterionOperator enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.conditionaliconcriterionoperator?view=office-scripts)
+	 */
+	export enum ConditionalIconCriterionOperator {
+		greaterThan,
+		greaterThanOrEqual,
+		invalid
+	}
+}
