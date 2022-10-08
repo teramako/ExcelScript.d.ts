cbecff3b23d63c30325087887ddbbca5c511c5b8
teramako <teramako@gmail.com>
2022-10-08 21:00:16 +0900

    enum BindingType

---
+++ enums/BindingType.d.ts
@@ -0,0 +1,10 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.BindingType enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.bindingtype?view=office-scripts)
+	 */
+	export enum BindingType {
+		range,
+		table,
+		text
+	}
+}
