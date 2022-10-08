341f38deb58d8798c574fe3912391bddf4b7ebe1
teramako <teramako@gmail.com>
2022-10-08 22:30:48 +0900

    enum DocumentPropertyType

---
+++ enums/DocumentPropertyType.d.ts
@@ -0,0 +1,12 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.DocumentPropertyType enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.documentpropertytype?view=office-scripts)
+	 */
+	export enum DocumentPropertyType {
+		boolean,
+		date,
+		float,
+		number,
+		string
+	}
+}
