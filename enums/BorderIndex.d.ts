8864c34f5d423cdfba1508c69294faec4c3e91c1
teramako <teramako@gmail.com>
2022-10-08 21:03:24 +0900

    enum BorderIndex

---
+++ enums/BorderIndex.d.ts
@@ -0,0 +1,15 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.BorderIndex enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.borderindex?view=office-scripts)
+	 */
+	export enum BorderIndex {
+		diagonalDown,
+		diagonalUp,
+		edgeBottom,
+		edgeLeft,
+		edgeRight,
+		edgeTop,
+		insideHorizontal,
+		insideVertical
+	}
+}
