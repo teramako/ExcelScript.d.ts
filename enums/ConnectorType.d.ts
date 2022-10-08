a319a86f5cc69a1c881ed8ac4b3d4523477518a7
teramako <teramako@gmail.com>
2022-10-08 22:20:35 +0900

    enum ConnectorType

---
+++ enums/ConnectorType.d.ts
@@ -0,0 +1,10 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.ConnectorType enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.connectortype?view=office-scripts)
+	 */
+	export enum ConnectorType {
+		curve,
+		elbow,
+		straight
+	}
+}
