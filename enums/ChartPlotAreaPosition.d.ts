ce0cacd94270138e972e25a4f4562e41d8303ad5
teramako <teramako@gmail.com>
2022-10-08 21:47:42 +0900

    enum ChartPlotAreaPosition

---
+++ enums/ChartPlotAreaPosition.d.ts
@@ -0,0 +1,9 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.ChartPlotAreaPosition enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.chartplotareaposition?view=office-scripts)
+	 */
+	export enum ChartPlotAreaPosition {
+		automatic,
+		custom
+	}
+}
