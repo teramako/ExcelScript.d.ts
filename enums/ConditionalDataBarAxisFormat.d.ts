5f4833c543590465eb12323d133fb2d6791b3903
teramako <teramako@gmail.com>
2022-10-08 22:04:36 +0900

    enum ConditionalDataBarAxisFormat

---
+++ enums/ConditionalDataBarAxisFormat.d.ts
@@ -0,0 +1,11 @@
+declare namespace ExcelScript {
+	/**
+	 * データ バー軸の書式オプションを表します。
+	 * @see [ExcelScript.ConditionalDataBarAxisFormat enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.conditionaldatabaraxisformat?view=office-scripts)
+	 */
+	export enum ConditionalDataBarAxisFormat {
+		automatic,
+		cellMidPoint,
+		none
+	}
+}
