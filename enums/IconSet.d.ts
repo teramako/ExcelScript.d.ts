1b8568d970495a230db32f56e2a4b0d3295deb00
teramako <teramako@gmail.com>
2022-10-08 22:40:07 +0900

    enum IconSet

---
+++ enums/IconSet.d.ts
@@ -0,0 +1,28 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.IconSet enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.iconset?view=office-scripts)
+	 */
+	export enum IconSet {
+		fiveArrows,
+		fiveArrowsGray,
+		fiveBoxes,
+		fiveQuarters,
+		fiveRating,
+		fourArrows,
+		fourArrowsGray,
+		fourRating,
+		fourRedToBlack,
+		fourTrafficLights,
+		invalid,
+		threeArrows,
+		threeArrowsGray,
+		threeFlags,
+		threeSigns,
+		threeStars,
+		threeSymbols,
+		threeSymbols2,
+		threeTrafficLights1,
+		threeTrafficLights2,
+		threeTriangles
+	}
+}
