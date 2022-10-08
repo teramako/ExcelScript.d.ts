9c1110aebd04f83251092602cb7882a5d7fda112
teramako <teramako@gmail.com>
2022-10-08 23:01:34 +0900

    enum PrintComments

---
+++ enums/PrintComments.d.ts
@@ -0,0 +1,19 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.PrintComments enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.printcomments?view=office-scripts)
+	 */
+	export enum PrintComments {
+		/**
+		 * コメントは、ワークシートの末尾に印刷されます。
+		 */
+		endSheet,
+		/**
+		 * コメントは、ワークシートの挿入された位置に印刷されます。
+		 */
+		inPlace,
+		/**
+		 * コメントは印刷されません。
+		 */
+		noComments
+	}
+}
