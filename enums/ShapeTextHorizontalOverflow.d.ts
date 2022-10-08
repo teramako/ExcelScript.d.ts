15e83d63bf3f7a36de9e9fa7971868fc56c513a8
teramako <teramako@gmail.com>
2022-10-08 23:27:28 +0900

    enum ShapeTextHorizontalOverflow

---
+++ enums/ShapeTextHorizontalOverflow.d.ts
@@ -0,0 +1,10 @@
+declare namespace ExcelScript {
+	/**
+	 * 図形のテキスト フレームの水平方向のオーバーフローを指定します。
+	 * @see [ExcelScript.ShapeTextHorizontalOverflow enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.shapetexthorizontaloverflow?view=office-scripts)
+	 */
+	export enum ShapeTextHorizontalOverflow {
+		clip,
+		overflow
+	}
+}
