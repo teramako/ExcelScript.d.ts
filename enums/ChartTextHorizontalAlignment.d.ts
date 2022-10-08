ff961a5ca164fd77c19434d7e0c0fdc6edfcc170
teramako <teramako@gmail.com>
2022-10-08 21:53:58 +0900

    enum ChartTextHorizontalAlignment

---
+++ enums/ChartTextHorizontalAlignment.d.ts
@@ -0,0 +1,13 @@
+declare namespace ExcelScript {
+	/**
+	 * 指定したオブジェクトの水平方向の配置を表します。
+	 * @see [ExcelScript.ChartTextHorizontalAlignment enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.charttexthorizontalalignment?view=office-scripts)
+	 */
+	export enum ChartTextHorizontalAlignment {
+		center,
+		distributed,
+		justify,
+		left,
+		right
+	}
+}
