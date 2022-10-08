e062a1bff6d7e95e375b26a314d0306f974826be
teramako <teramako@gmail.com>
2022-10-08 21:23:08 +0900

    enum ChartAxisType

---
+++ enums/ChartAxisType.d.ts
@@ -0,0 +1,20 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.ChartAxisType enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.chartaxistype?view=office-scripts)
+	 */
+	export enum ChartAxisType {
+		/**
+		 * 軸は項目を表示します。
+		 */
+		category,
+		invalid,
+		/**
+		 * 軸はデータ系列を表示します。
+		 */
+		series,
+		/**
+		 * 軸は値を表示します。
+		 */
+		value
+	}
+}
