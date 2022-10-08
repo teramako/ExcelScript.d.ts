391bd76da553668f3d029b95313b1348b7e09eb6
teramako <teramako@gmail.com>
2022-10-08 23:18:06 +0900

    enum SearchDirection

---
+++ enums/SearchDirection.d.ts
@@ -0,0 +1,16 @@
+declare namespace ExcelScript {
+	/**
+	 * 検索の方向を指定します。
+	 * @see [ExcelScript.SearchDirection enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.searchdirection?view=office-scripts)
+	 */
+	export enum SearchDirection {
+		/**
+		 * 逆の順序で検索します。
+		 */
+		backwards,
+		/**
+		 * 順方向に検索します。
+		 */
+		forward
+	}
+}
