d0ec5d29faa49e44c52752eb38c619999bc37751
teramako <teramako@gmail.com>
2022-10-08 23:26:28 +0900

    enum ShapeTextHorizontalAlignment

---
+++ enums/ShapeTextHorizontalAlignment.d.ts
@@ -0,0 +1,15 @@
+declare namespace ExcelScript {
+	/**
+	 * 図形のテキスト フレームの水平方向の配置を指定します。
+	 * @see [ExcelScript.ShapeTextHorizontalAlignment enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.shapetexthorizontalalignment?view=office-scripts)
+	 */
+	export enum ShapeTextHorizontalAlignment {
+		center,
+		distributed,
+		justify,
+		justifyLow,
+		left,
+		right,
+		thaiDistributed
+	}
+}
