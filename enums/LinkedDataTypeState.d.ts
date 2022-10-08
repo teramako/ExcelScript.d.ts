9aee20b8a53f557b77790bd7f84dde7803f68fb0
teramako <teramako@gmail.com>
2022-10-08 22:47:27 +0900

    enum LinkedDataTypeState

---
+++ enums/LinkedDataTypeState.d.ts
@@ -0,0 +1,12 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.LinkedDataTypeState enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.linkeddatatypestate?view=office-scripts)
+	 */
+	export enum LinkedDataTypeState {
+		brokenLinkedData,
+		disambiguationNeeded,
+		fetchingData,
+		none,
+		validLinkedData
+	}
+}
