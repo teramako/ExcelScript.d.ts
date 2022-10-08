51d569837621c8f0abad417e29c58b055d9ed9ef
teramako <teramako@gmail.com>
2022-10-08 21:38:56 +0900

    enum ChartGradientStyleType

---
+++ enums/ChartGradientStyleType.d.ts
@@ -0,0 +1,12 @@
+declare namespace ExcelScript {
+	/**
+	 * グラフ系列のグラデーション スタイルの種類を表します。
+	 * これは地域マップ グラフにのみ適用されます。
+	 * @see [ExcelScript.ChartGradientStyleType enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.chartgradientstyletype?view=office-scripts)
+	 */
+	export enum ChartGradientStyleType {
+		extremeValue,
+		number,
+		percent
+	}
+}
