f43b26bacc79cac090f7e0e0131ba3995eb132b4
teramako <teramako@gmail.com>
2022-10-08 21:17:08 +0900

    enum ChartAxisPosition

---
+++ enums/ChartAxisPosition.d.ts
@@ -0,0 +1,11 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.ChartAxisPosition enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.chartaxisposition?view=office-scripts)
+	 */
+	export enum ChartAxisPosition {
+		automatic,
+		custom,
+		maximum,
+		minimum
+	}
+}
