e14ab6d15540a402d1fb3e9cce54a5c2df00b44f
teramako <teramako@gmail.com>
2022-10-08 22:00:23 +0900

    enum ChartUnderlineStyle

---
+++ enums/ChartUnderlineStyle.d.ts
@@ -0,0 +1,9 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.ChartUnderlineStyle enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.chartunderlinestyle?view=office-scripts)
+	 */
+	export enum ChartUnderlineStyle {
+		none,
+		single
+	}
+}
