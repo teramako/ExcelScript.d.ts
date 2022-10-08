7c84ff982da9a181b7e13e9a004e0b5b906ff4f3
teramako <teramako@gmail.com>
2022-10-08 22:53:18 +0900

    enum PaperType

---
+++ enums/PaperType.d.ts
@@ -0,0 +1,48 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.PaperType enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.papertype?view=office-scripts)
+	 */
+	export enum PaperType {
+		a3,
+		a4,
+		a4Small,
+		a5,
+		b4,
+		b5,
+		csheet,
+		dsheet,
+		envelope10,
+		envelope11,
+		envelope12,
+		envelope14,
+		envelope9,
+		envelopeB4,
+		envelopeB5,
+		envelopeB6,
+		envelopeC3,
+		envelopeC4,
+		envelopeC5,
+		envelopeC6,
+		envelopeC65,
+		envelopeDL,
+		envelopeItaly,
+		envelopeMonarch,
+		envelopePersonal,
+		esheet,
+		executive,
+		fanfoldLegalGerman,
+		fanfoldStdGerman,
+		fanfoldUS,
+		folio,
+		ledger,
+		legal,
+		letter,
+		letterSmall,
+		note,
+		paper10x14,
+		paper11x17,
+		quatro,
+		statement,
+		tabloid
+	}
+}
