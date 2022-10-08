87fa1278e03e4461c542e6041f9288d44e60f35e
teramako <teramako@gmail.com>
2022-10-08 22:51:53 +0900

    enum PageOrientation

---
+++ enums/PageOrientation.d.ts
@@ -0,0 +1,9 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.PageOrientation enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.pageorientation?view=office-scripts)
+	 */
+	export enum PageOrientation {
+		landscape,
+		portrait
+	}
+}
