a1de655ca8ef986f179ed11ea7dcaab5e6f3a7db
teramako <teramako@gmail.com>
2022-10-08 22:30:00 +0900

    enum DeleteShiftDirection

---
+++ enums/DeleteShiftDirection.d.ts
@@ -0,0 +1,9 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.DeleteShiftDirection enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.deleteshiftdirection?view=office-scripts)
+	 */
+	export enum DeleteShiftDirection {
+		left,
+		up
+	}
+}
