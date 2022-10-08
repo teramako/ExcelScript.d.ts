bce0dbee5cdea62c350ccd582fda16ad2745510b
teramako <teramako@gmail.com>
2022-10-08 22:41:01 +0900

    enum ImageFittingMode

---
+++ enums/ImageFittingMode.d.ts
@@ -0,0 +1,10 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.ImageFittingMode enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.imagefittingmode?view=office-scripts)
+	 */
+	export enum ImageFittingMode {
+		fill,
+		fit,
+		fitAndCenter
+	}
+}
