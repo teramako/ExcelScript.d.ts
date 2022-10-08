92bbec7429bafa4cc7d619f66ad1268f16ae5d78
teramako <teramako@gmail.com>
2022-10-08 23:33:38 +0900

    enum ShapeZOrder

---
+++ enums/ShapeZOrder.d.ts
@@ -0,0 +1,12 @@
+declare namespace ExcelScript {
+	/**
+	 * 他の図形を基準として、図形を移動する位置を Z オーダーで指定します。
+	 * @see [ExcelScript.ShapeZOrder enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.shapezorder?view=office-scripts)
+	 */
+	export enum ShapeZOrder {
+		bringForward,
+		bringToFront,
+		sendBackward,
+		sendToBack
+	}
+}
