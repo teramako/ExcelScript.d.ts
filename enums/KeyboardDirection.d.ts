99bbaaa90778fa6d3b31b17c5af48ee860ebf6df
teramako <teramako@gmail.com>
2022-10-08 22:43:03 +0900

    enum KeyboardDirection

---
+++ enums/KeyboardDirection.d.ts
@@ -0,0 +1,11 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.KeyboardDirection enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.keyboarddirection?view=office-scripts)
+	 */
+	export enum KeyboardDirection {
+		down,
+		left,
+		right,
+		up
+	}
+}
