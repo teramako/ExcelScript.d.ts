8cd49fefc5ee2778ea7a26ff378aea0d9189699e
teramako <teramako@gmail.com>
2022-10-08 23:34:29 +0900

    enum SheetVisibility

---
+++ enums/SheetVisibility.d.ts
@@ -0,0 +1,10 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.SheetVisibility enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.sheetvisibility?view=office-scripts)
+	 */
+	export enum SheetVisibility {
+		hidden,
+		veryHidden,
+		visible
+	}
+}
