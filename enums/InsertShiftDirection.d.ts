a871ad46b1a4e062b96c46f769a0ae2123498a4f
teramako <teramako@gmail.com>
2022-10-08 22:42:02 +0900

    enum InsertShiftDirection

---
+++ enums/InsertShiftDirection.d.ts
@@ -0,0 +1,9 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.InsertShiftDirection enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.insertshiftdirection?view=office-scripts)
+	 */
+	export enum InsertShiftDirection {
+		down,
+		right
+	}
+}
