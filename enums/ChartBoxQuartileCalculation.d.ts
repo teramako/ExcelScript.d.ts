d4b4fe16e8c415fedd878fa4c3c3849d6fe5e0db
teramako <teramako@gmail.com>
2022-10-08 21:26:20 +0900

    enum ChartBoxQuartileCalculation

---
+++ enums/ChartBoxQuartileCalculation.d.ts
@@ -0,0 +1,10 @@
+declare namespace ExcelScript {
+	/**
+	 * グラフ系列レイアウトの四分位計算の種類を表します。 ボックスとひげグラフにのみ適用されます。
+	 * @see [ExcelScript.ChartBoxQuartileCalculation enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.chartboxquartilecalculation?view=office-scripts)
+	 */
+	export enum ChartBoxQuartileCalculation {
+		exclusive,
+		inclusive
+	}
+}
