2c08887bce89e1d5616067af2028a9b404b4b432
teramako <teramako@gmail.com>
2022-10-08 22:16:57 +0900

    enum ConditionalRangeFontUnderlineStyle

---
+++ enums/ConditionalRangeFontUnderlineStyle.d.ts
@@ -0,0 +1,10 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.ConditionalRangeFontUnderlineStyle enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.conditionalrangefontunderlinestyle?view=office-scripts)
+	 */
+	export enum ConditionalRangeFontUnderlineStyle {
+		double,
+		none,
+		singile
+	}
+}
