dd556185382299b381978abfed726f6c6878a1b3
teramako <teramako@gmail.com>
2022-10-08 22:21:37 +0900

    enum ContentType

---
+++ enums/ContentType.d.ts
@@ -0,0 +1,15 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.ContentType enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.contenttype?view=office-scripts)
+	 */
+	export enum ContentType {
+		/**
+		 * メンションを含むコメント コンテンツ。
+		 */
+		mention,
+		/**
+		 * コメント コンテンツのプレーンな形式の種類を示します。
+		 */
+		plain
+	}
+}
