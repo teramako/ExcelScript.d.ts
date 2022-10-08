0a3bb468a58c43dd2e7fdc9fa9f289799f04b86e
teramako <teramako@gmail.com>
2022-10-08 21:39:58 +0900

    enum ChartLegendPosition

---
+++ enums/ChartLegendPosition.d.ts
@@ -0,0 +1,14 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.ChartLegendPosition enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.chartlegendposition?view=office-scripts)
+	 */
+	export enum ChartLegendPosition {
+		bottom,
+		corner,
+		custom,
+		invalid,
+		left,
+		right,
+		top
+	}
+}
