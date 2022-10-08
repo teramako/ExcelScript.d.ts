5f0b94f9ecb380417d5bdc0a8e7798e9217d06bf
teramako <teramako@gmail.com>
2022-10-08 23:39:49 +0900

    enum SortMethod

---
+++ enums/SortMethod.d.ts
@@ -0,0 +1,9 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.SortMethod enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.sortmethod?view=office-scripts)
+	 */
+	export enum SortMethod {
+		pinYin,
+		strokeCount
+	}
+}
