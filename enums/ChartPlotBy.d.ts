bd891c76d28e551a9ad4d2a4801be59d55cc880d
teramako <teramako@gmail.com>
2022-10-08 21:48:36 +0900

    enum ChartPlotBy

---
+++ enums/ChartPlotBy.d.ts
@@ -0,0 +1,9 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.ChartPlotBy enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.chartplotby?view=office-scripts)
+	 */
+	export enum ChartPlotBy {
+		columns,
+		rows
+	}
+}
