b1888c14e8ed8730688278138b73993b95f7c572
teramako <teramako@gmail.com>
2022-10-08 23:22:18 +0900

    enum ShapeLineDashStyle

---
+++ enums/ShapeLineDashStyle.d.ts
@@ -0,0 +1,20 @@
+declare namespace ExcelScript {
+	/**
+	 * 線のダッシュ スタイル。
+	 * @see [ExcelScript.ShapeLineDashStyle enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.shapelinedashstyle?view=office-scripts)
+	 */
+	export enum ShapeLineDashStyle {
+		dash,
+		dashDot,
+		dashDotDot,
+		longDash,
+		longDashDot,
+		longDashDotDot,
+		roundDot,
+		solid,
+		squareDot,
+		systemDash,
+		systemDashDot,
+		systemDot
+	}
+}
