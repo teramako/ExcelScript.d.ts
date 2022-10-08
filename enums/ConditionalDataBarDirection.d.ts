a210121053a7cf82138827cca2330368285a6f6a
teramako <teramako@gmail.com>
2022-10-08 22:05:55 +0900

    enum ConditionalDataBarDirection

---
+++ enums/ConditionalDataBarDirection.d.ts
@@ -0,0 +1,11 @@
+declare namespace ExcelScript {
+	/**
+	 * セル内のデータ バーの方向を表します。
+	 * @see [ExcelScript.ConditionalDataBarDirection enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.conditionaldatabardirection?view=office-scripts)
+	 */
+	export enum ConditionalDataBarDirection {
+		context,
+		leftToRight,
+		rightToLeft
+	}
+}
