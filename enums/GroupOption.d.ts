dfeefd111aa80446fd15c38a7a9a4b05116058d1
teramako <teramako@gmail.com>
2022-10-08 22:37:21 +0900

    enum GroupOption

---
+++ enums/GroupOption.d.ts
@@ -0,0 +1,15 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.GroupOption enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.groupoption?view=office-scripts)
+	 */
+	export enum GroupOption {
+		/**
+		 * 列でグループ化します。
+		 */
+		byColumns,
+		/**
+		 * 行でグループ化します。
+		 */
+		byRows
+	}
+}
