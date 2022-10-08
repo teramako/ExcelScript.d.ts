9e3e66deaae56122ae49412f3f270e579df481af
teramako <teramako@gmail.com>
2022-10-08 21:40:58 +0900

    enum ChartLineStyle

---
+++ enums/ChartLineStyle.d.ts
@@ -0,0 +1,18 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.ChartLineStyle enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.chartlinestyle?view=office-scripts)
+	 */
+	export enum ChartLineStyle {
+		automatic,
+		continuous,
+		dash,
+		dashDot,
+		dashDotDot,
+		dot,
+		grey25,
+		grey50,
+		grey75,
+		none,
+		roundDot
+	}
+}
