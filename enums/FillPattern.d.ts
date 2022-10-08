8ae8b062731ba33b65ba525e1bf269caa9ce70c3
teramako <teramako@gmail.com>
2022-10-08 22:32:25 +0900

    enum FillPattern

---
+++ enums/FillPattern.d.ts
@@ -0,0 +1,28 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.FillPattern enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.fillpattern?view=office-scripts)
+	 */
+	export enum FillPattern {
+		checker,
+		crissCross,
+		down,
+		gray16,
+		gray25,
+		gray50,
+		gray75,
+		gray8,
+		grid,
+		horizontal,
+		lightDown,
+		lightHorizontal,
+		lightUp,
+		lightVertical,
+		linearGradient,
+		none,
+		rectangularGradient,
+		semiGray75,
+		solid,
+		up,
+		vertical
+	}
+}
