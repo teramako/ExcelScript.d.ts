7bb922afdef5ebee1341a1eb1cf01c2447f2fb68
teramako <teramako@gmail.com>
2022-10-08 21:04:31 +0900

    enum BorderLineStyle

---
+++ enums/BorderLineStyle.d.ts
@@ -0,0 +1,15 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.BorderLineStyle enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.borderlinestyle?view=office-scripts)
+	 */
+	export enum BorderLineStyle {
+		continuous,
+		dash,
+		dashDot,
+		dashDotDot,
+		dot,
+		double,
+		none,
+		slantDashDot
+	}
+}
