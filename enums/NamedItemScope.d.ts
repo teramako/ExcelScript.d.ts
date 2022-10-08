94aa7683da6d002fb16008d8a24977a66c9d9f86
teramako <teramako@gmail.com>
2022-10-08 22:48:12 +0900

    enum NamedItemScope

---
+++ enums/NamedItemScope.d.ts
@@ -0,0 +1,9 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.NamedItemScope enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.nameditemscope?view=office-scripts)
+	 */
+	export enum NamedItemScope {
+		workbook,
+		worksheet
+	}
+}
