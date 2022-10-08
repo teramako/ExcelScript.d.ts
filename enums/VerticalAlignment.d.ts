cacf62e1286cebce598bc320623e5d0c67440425
teramako <teramako@gmail.com>
2022-10-08 23:51:38 +0900

    enum VerticalAlignment

---
+++ enums/VerticalAlignment.d.ts
@@ -0,0 +1,12 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.VerticalAlignment enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.verticalalignment?view=office-scripts)
+	 */
+	export enum VerticalAlignment {
+		bottom,
+		center,
+		distributed,
+		justify,
+		top
+	}
+}
