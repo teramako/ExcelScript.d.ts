ede05593eceaafde664abec4bba37e7340927024
teramako <teramako@gmail.com>
2022-10-08 21:21:43 +0900

    enum ChartAxisTimeUnit

---
+++ enums/ChartAxisTimeUnit.dt.ts
@@ -0,0 +1,11 @@
+declare namespace ExcelScript {
+	/**
+	 * グラフ軸とデータ系列の時間の単位を指定します。
+	 * @see [ExcelScript.ChartAxisTimeUnit enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.chartaxistimeunit?view=office-scripts)
+	 */
+	export enum ChartAxisTimeUnit {
+		days,
+		months,
+		years
+	}
+}
