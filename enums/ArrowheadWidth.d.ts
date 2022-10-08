7583b4cded82ad8c429925e3eb4d403edb4e2e1c
teramako <teramako@gmail.com>
2022-10-08 20:48:27 +0900

    enum ArrowheadWidth

---
+++ enums/ArrowheadWidth.d.ts
@@ -0,0 +1,10 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.ArrowheadWidth enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.arrowheadwidth?view=office-scripts)
+	 */
+	export enum ArrowheadWidth {
+		medium,
+		narrow,
+		wide
+	}
+}
