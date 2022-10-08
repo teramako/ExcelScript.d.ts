d072cf8dfcb745c3fa26d89565fc1270e5703f9d
teramako <teramako@gmail.com>
2022-10-08 23:07:20 +0900

    enum RangeValueType

---
+++ enums/RangeValueType.d.ts
@@ -0,0 +1,15 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.RangeValueType enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.rangevaluetype?view=office-scripts)
+	 */
+	export enum RangeValueType {
+		boolean,
+		double,
+		empty,
+		error,
+		integer,
+		richValue,
+		string,
+		unknown
+	}
+}
