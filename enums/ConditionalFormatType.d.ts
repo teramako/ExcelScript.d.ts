b5ddca6e1412a764624a23635d13d8181f6dd035
teramako <teramako@gmail.com>
2022-10-08 22:13:11 +0900

    enum ConditionalFormatType

---
+++ enums/ConditionalFormatType.d.ts
@@ -0,0 +1,15 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.ConditionalFormatType enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.conditionalformattype?view=office-scripts)
+	 */
+	export enum ConditionalFormatType {
+		cellValue,
+		colorScale,
+		containsText,
+		custom,
+		dataBar,
+		iconSet,
+		presetCriteria,
+		topBottom
+	}
+}
