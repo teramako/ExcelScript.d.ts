5ee687e203bf696d5d146f26274c974bc5d24a7a
teramako <teramako@gmail.com>
2022-10-08 23:06:27 +0900

    enum RangeUnderlineStyle

---
+++ enums/RangeUnderlineStyle.d.ts
@@ -0,0 +1,12 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.RangeUnderlineStyle enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.rangeunderlinestyle?view=office-scripts)
+	 */
+	export enum RangeUnderlineStyle {
+		double,
+		doubleAccountant,
+		none,
+		single,
+		singleAccountant
+	}
+}
