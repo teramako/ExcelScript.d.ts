12042b578e1cd2b5c22481772245aa9b728edec4
teramako <teramako@gmail.com>
2022-10-08 23:40:46 +0900

    enum SortOn

---
+++ enums/SortOn.d.ts
@@ -0,0 +1,11 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.SortOn enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.sorton?view=office-scripts)
+	 */
+	export enum SortOn {
+		cellColor,
+		fontColor,
+		icon,
+		value
+	}
+}
