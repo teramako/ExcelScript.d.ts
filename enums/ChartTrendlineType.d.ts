522bdd8b0874454626836d1d95ac9cbb240f59bd
teramako <teramako@gmail.com>
2022-10-08 21:57:46 +0900

    enum ChartTrendlineType

---
+++ enums/ChartTrendlineType.d.ts
@@ -0,0 +1,13 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.ChartTrendlineType enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.charttrendlinetype?view=office-scripts)
+	 */
+	export enum ChartTrendlineType {
+		exponential,
+		linear,
+		logarithmic,
+		movingAverage,
+		polynomial,
+		power
+	}
+}
