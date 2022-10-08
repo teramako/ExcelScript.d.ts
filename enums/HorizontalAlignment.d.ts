c188030b0a70dd402592bd12f2a415e8261ea437
teramako <teramako@gmail.com>
2022-10-08 22:39:20 +0900

    enum HorizontalAlignment

---
+++ enums/HorizontalAlignment.d.ts
@@ -0,0 +1,15 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.HorizontalAlignment enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.horizontalalignment?view=office-scripts)
+	 */
+	export enum HorizontalAlignment {
+		center,
+		centerAcrossSelection,
+		distributed,
+		fill,
+		general,
+		justify,
+		left,
+		right
+	}
+}
