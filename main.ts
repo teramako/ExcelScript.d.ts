94f09131e8d2b88212fcd34b87041604ff893858
teramako <teramako@gmail.com>
2022-10-10 19:07:06 +0900

    function run

---
+++ main.ts
@@ -0,0 +1,8 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.run(callback)](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript?view=office-scripts#excelscript-excelscript-run-function(1))
+	 */
+	export function run(
+		callback: (workbook: Workbook) => Promise<void>,
+	): Promise<void>;
+}
