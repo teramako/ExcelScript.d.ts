d612f135499dfd24c30b1820a59d83503f35770a
teramako <teramako@gmail.com>
2022-10-10 18:48:39 +0900

    type RangeValue

---
+++ types/RangeValue.d.ts
@@ -0,0 +1,6 @@
+declare namespace ExcelScript {
+	/**
+	 * @see [ExcelScript.RangeValue type](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.rangevalue?view=office-scripts)
+	 */
+	export type RangeValue = string | number | boolean | null | undefined;
+}
