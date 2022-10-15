a8d4c743c0a5dae5c07b095ef03700203ee1ddb5
teramako <teramako@gmail.com>
2022-10-15 23:28:34 +0900

    add example

--- enums/ClearApplyTo.d.ts
+++ enums/ClearApplyTo.d.ts
@@ -1,6 +1,24 @@
 declare namespace ExcelScript {
 	/**
 	 * @see [ExcelScript.ClearApplyTo enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.clearapplyto?view=office-scripts)
+	 *
+	 * @example
+	 * ```
+	 * // This script removes any extra formatting that's been applied to a table.
+	 * // This leaves only the base table style effects.
+	 * // Any formatting outside of the table will be left as is.
+	 * function main(workbook: ExcelScript.Workbook) {
+	 *   // Get the first table on the current worksheet.
+	 *   let worksheet = workbook.getActiveWorksheet();
+	 *   let table = worksheet.getTables()[0];
+	 *
+	 *   // Get the range used by the table.
+	 *   let range = table.getRange();
+	 *
+	 *   // Clear all the formatting that is not applied by the table and the table style.
+	 *   range.clear(ExcelScript.ClearApplyTo.formats);
+	 * }
+	 * ```
 	 */
 	export enum ClearApplyTo {
 		all,
