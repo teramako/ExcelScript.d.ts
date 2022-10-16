b738d80de0410b1c6c66c163f73e9b2f23dc2811
teramako <teramako@gmail.com>
2022-10-16 17:20:11 +0900

    add example

--- enums/SheetVisibility.d.ts
+++ enums/SheetVisibility.d.ts
@@ -1,6 +1,18 @@
 declare namespace ExcelScript {
 	/**
 	 * @see [ExcelScript.SheetVisibility enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.sheetvisibility?view=office-scripts)
+	 *
+	 * @example
+	 * ```
+	 * // This script unhides all the worksheets in the workbook.
+	 * function main(workbook: ExcelScript.Workbook) {
+	 *   // Iterate over each worksheet.
+	 *   workbook.getWorksheets().forEach((worksheet) => {
+	 *     // Set the worksheet visibility to visible.
+	 *     worksheet.setVisibility(ExcelScript.SheetVisibility.visible);
+	 *   });
+	 * }
+	 * ```
 	 */
 	export enum SheetVisibility {
 		hidden,
