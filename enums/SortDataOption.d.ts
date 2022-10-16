7541efcbe8b77444c5eb54d853c44e0661e1a490
teramako <teramako@gmail.com>
2022-10-16 17:24:06 +0900

    add example

--- enums/SortDataOption.d.ts
+++ enums/SortDataOption.d.ts
@@ -1,6 +1,30 @@
 declare namespace ExcelScript {
 	/**
 	 * @see [ExcelScript.SortDataOption enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.sortdataoption?view=office-scripts)
+	 *
+	 * @example
+	 * ```
+	 * // This script sorts a table based on the values in column 1.
+	 * // If the text of a column-1 value can be treated as a number,
+	 * // it will be sorted in numerical order, rather than Unicode order
+	 * // (so 123 will come before 12.3).
+	 * function main(workbook: ExcelScript.Workbook) {
+	 *   // Get the first table on the current worksheet.
+	 *   const currentSheet = workbook.getActiveWorksheet();
+	 *   const table = currentSheet.getTables()[0];
+	 *
+	 *   // Create the sorting parameters.
+	 *   const countSortField: ExcelScript.SortField = {
+	 *     key: 1,
+	 *     ascending: true,
+	 *     dataOption: ExcelScript.SortDataOption.textAsNumber
+	 *   };
+	 *
+	 *   // Apply the sort to the table.
+	 *   const sort = table.getSort();
+	 *   sort.apply([countSortField]);
+	 * }
+	 * ```
 	 */
 	export enum SortDataOption {
 		normal,
--- enums/SortMethod.d.ts
+++ enums/SortMethod.d.ts
@@ -1,6 +1,32 @@
 declare namespace ExcelScript {
 	/**
 	 * @see [ExcelScript.SortMethod enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.sortmethod?view=office-scripts)
+	 *
+	 * @example
+	 * ```
+	 * // This script sorts a range using the values in the first column.
+	 * function main(workbook: ExcelScript.Workbook) {
+	 *   // Get the range (A1:D8) to sort from the current worksheet.
+	 *   const worksheet = workbook.getActiveWorksheet();
+	 *   const rangeToSort = worksheet.getRange("A1:D8");
+	 *
+	 *   // Create a SortField for text sorting.
+	 *   let valueSort: ExcelScript.SortField = {
+	 *     ascending: true,
+	 *     key: 0, // First column
+	 *     sortOn: ExcelScript.SortOn.value
+	 *   };
+	 *
+	 *   // Apply the SortField to the range.
+	 *   rangeToSort.getSort().apply(
+	 *     [valueSort],
+	 *     false, // Don't let casing have an impact of sorting.
+	 *     true, // The range has headers.
+	 *     ExcelScript.SortOrientation.rows,
+	 *     ExcelScript.SortMethod.pinYin // Use phonetic sorting for Chinese characters.
+	 *     );
+	 * }
+	 * ```
 	 */
 	export enum SortMethod {
 		pinYin,
