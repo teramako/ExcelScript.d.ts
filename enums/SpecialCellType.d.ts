aaaf5d68cdf3efaca2fbe82ee06f12514c2e8768
teramako <teramako@gmail.com>
2022-10-16 17:26:31 +0900

    add example

--- enums/SpecialCellType.d.ts
+++ enums/SpecialCellType.d.ts
@@ -1,6 +1,22 @@
 declare namespace ExcelScript {
 	/**
 	 * @see [ExcelScript.SpecialCellType enum](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.specialcelltype?view=office-scripts)
+	 *
+	 * @example
+	 * ```
+	 * // This script finds and highlights all the cells in the current worksheet that contain a formula.
+	 * function main(workbook: ExcelScript.Workbook) {
+	 *   // Get the used range on the current sheet.
+	 *   const currentSheet = workbook.getActiveWorksheet();
+	 *   const usedRange = currentSheet.getUsedRange();
+	 *
+	 *   // Get the RangeAreas object for each cell with a formula.
+	 *   const formulaCells = usedRange.getSpecialCells(ExcelScript.SpecialCellType.formulas);
+	 *
+	 *   // Add a light blue background to the cells containing formulas.
+	 *   formulaCells.getFormat().getFill().setColor("#ADD8E6");
+	 * }
+	 * ```
 	 */
 	export enum SpecialCellType {
 		/**
