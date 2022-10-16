041350c51cdb0a014a38942abb9e704365d95899
teramako <teramako@gmail.com>
2022-10-16 17:41:44 +0900

    add example for AutoFilter

--- interfaces/AutoFilter.d.ts
+++ interfaces/AutoFilter.d.ts
@@ -1,9 +1,30 @@
 /// <reference path="./FilterCriteria.d.ts"/>
+/// <reference path="./Range.d.ts" />
 declare namespace ExcelScript {
 	/**
 	 * オブジェクトを `AutoFilter` 表します。
 	 * AutoFilter は、セルの内容に基づいて、Excel列の値を特定のフィルターに変換します。
 	 * @see [ExcelScript.AutoFilter interface](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.autofilter?view=office-scripts)
+	 *
+	 * @example
+	 * ```
+	 * // This script creates an autoFilter on the worksheet that filters out rows based on column values.
+	 * // The autoFilter filters to only include rows that have a value in column C in the lowest 10 values
+	 * // (of column C values).
+	 * function main(workbook: ExcelScript.Workbook) {
+	 *   // Get the autoFilter of the first table in the current worksheet.
+	 *   const currentSheet = workbook.getActiveWorksheet();
+	 *   const dataRange = currentSheet.getUsedRange();
+	 *   const autoFilter = currentSheet.getAutoFilter();
+	 *
+	 *   // Add a filter that will only show the rows with the lowest 10 values in column C
+	 *   // (index 2, assuming the used range spans from at least A:C).
+	 *   autoFilter.apply(dataRange, 2, {
+	 *     criterion1: "10",
+	 *     filterOn: ExcelScript.FilterOn.bottomItems
+	 *   });
+	 * }
+	 * ```
 	 */
 	export interface AutoFilter {
 		/**
@@ -11,6 +32,26 @@ declare namespace ExcelScript {
 		 * @param range オートフィルターが適用される範囲。
 		 * @param columnIndex オートフィルターが適用される 0 から始まる列インデックス。
 		 * @param criteria フィルター条件。
+		 *
+		 * @example
+		 * ```
+		 * // This script applies a filter to a table so that
+		 * // only rows with values in column 1 that start with "L" are shown.
+		 * function main(workbook: ExcelScript.Workbook) {
+		 *   // Get the autoFilter of the first table in the current worksheet.
+		 *   const table = workbook.getActiveWorksheet().getTables()[0];
+		 *   const autoFilter = table.getAutoFilter();
+		 *
+		 *   // Filter to only include values that start with "L".
+		 *   const filterCriteria: ExcelScript.FilterCriteria = {
+		 *     filterOn: ExcelScript.FilterOn.custom,
+		 *     criterion1: "L*"
+		 *   };
+		 *
+		 *   // Apply the filter to column 1 (zero-based).
+		 *   autoFilter.apply(table.getRange(), 1, filterCriteria);
+		 * }
+		 * ```
 		 */
 		apply(
 			range: Range | string,
