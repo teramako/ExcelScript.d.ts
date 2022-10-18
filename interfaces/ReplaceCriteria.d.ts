cee8ea4bf3bfc9d6dd505e1a3f59638ad6c135b9
teramako <teramako@gmail.com>
2022-10-18 17:36:18 +0900

    add examples for Replace/SearchCriteria

--- interfaces/ReplaceCriteria.d.ts
+++ interfaces/ReplaceCriteria.d.ts
@@ -9,11 +9,51 @@ declare namespace ExcelScript {
 		 * 完全一致は、セルの内容全体と一致します。
 		 * 部分的な一致は、セルのコンテンツ内の部分文字列と一致します ( `cat` `caterpillar` 部分的に一致し、 `scatter`).
 		 * 既定値は `false` (部分) です。
+		 *
+		 * @example
+		 * ```
+		 * // This script normalizes the text in a column so that values don't include both "OK" and "okay".
+		 * // It replaces "OK" and all the case-based variants with "okay".
+		 * function main(workbook: ExcelScript.Workbook) {
+		 *   // Get the range representing column D.
+		 *   const currentSheet = workbook.getActiveWorksheet();
+		 *   const column = currentSheet.getRange("D:D");
+		 *
+		 *   // Create a ReplaceCriteria object for the Range.replaceAll call.
+		 *   const criteria: ExcelScript.ReplaceCriteria = {
+		 *     completeMatch: true, // Use a complete match to skip cells that already say "okay".
+		 *     matchCase: false // Ignore case when comparing strings.
+		 *   };
+		 *
+		 *   // Replace all instances of "ok" (case-insensitive) with "okay".
+		 *   column.replaceAll("ok", "okay", criteria);
+		 * }
+		 * ```
 		 */
 		completeMatch?: boolean;
 		/**
 		 * 一致で大文字と小文字が区別される場合を指定します。
 		 * 既定値は `false` (大文字と小文字を区別しない) です。
+		 *
+		 * @example
+		 * ```
+		 * // This script replaces instances of "NA" with "North America",
+		 * // using the casing to ignore parts of words.
+		 * function main(workbook: ExcelScript.Workbook) {
+		 *   // Get the currently used range.
+		 *   const currentSheet = workbook.getActiveWorksheet();
+		 *   const usedRange = currentSheet.getUsedRange();
+		 *
+		 *   // Create a ReplaceCriteria object for the Range.replaceAll call.
+		 *   const criteria: ExcelScript.ReplaceCriteria = {
+		 *     completeMatch: false,
+		 *     matchCase: true // Match with "NA market", not "navigate"
+		 *   }
+		 *
+		 *   // Replace all instances of "NA" (case-sensitive) with "North America".
+		 *   usedRange.replaceAll("NA", "North America", criteria);
+		 * }
+		 * ```
 		 */
 		matchCase?: boolean;
 	}
