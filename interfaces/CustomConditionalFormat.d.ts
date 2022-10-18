3d7355d0a909224d8adbd184188c0932e6e3df68
teramako <teramako@gmail.com>
2022-10-18 15:27:57 +0900

    add example for CustomConditionalFormat

--- interfaces/CustomConditionalFormat.d.ts
+++ interfaces/CustomConditionalFormat.d.ts
@@ -3,6 +3,31 @@ declare namespace ExcelScript {
 	/**
 	 * ユーザー設定の条件付き書式の種類を表します。
 	 * @see [ExcelScript.CustomConditionalFormat interface](https://learn.microsoft.com/ja-jp/javascript/api/office-scripts/excelscript/excelscript.customconditionalformat?view=office-scripts)
+	 *
+	 * @example
+	 * ```
+	 * // This script applies a custom three-color conditional formatting to the selected range.
+	 * // The three colors represent positive, negative, or no changes from the values in the previous column.
+	 * function main(workbook: ExcelScript.Workbook) {
+	 *   // Get the selected cells.
+	 *   let selectedRange = workbook.getSelectedRange();
+	 *
+	 *   // Apply a rule for positive change from the previous column.
+	 *   let positiveChange = selectedRange.addConditionalFormat(ExcelScript.ConditionalFormatType.custom);
+	 *   positiveChange.getCustom().getFormat().getFill().setColor("lightgreen");
+	 *   positiveChange.getCustom().getRule().setFormula(`=${selectedRange.getCell(0, 0).getAddress()}>${selectedRange.getOffsetRange(0, -1).getCell(0, 0).getAddress()}`);
+	 *
+	 *   // Apply a rule for negative change from the previous column.
+	 *   let negativeChange = selectedRange.addConditionalFormat(ExcelScript.ConditionalFormatType.custom);
+	 *   negativeChange.getCustom().getFormat().getFill().setColor("pink");
+	 *   negativeChange.getCustom().getRule().setFormula(`=${selectedRange.getCell(0, 0).getAddress()}<${selectedRange.getOffsetRange(0, -1).getCell(0, 0).getAddress()}`);
+	 *
+	 *   // Apply a rule for no change from the previous column.
+	 *   let noChange = selectedRange.addConditionalFormat(ExcelScript.ConditionalFormatType.custom);
+	 *   noChange.getCustom().getFormat().getFill().setColor("lightyellow");
+	 *   noChange.getCustom().getRule().setFormula(`=${selectedRange.getCell(0, 0).getAddress()}=${selectedRange.getOffsetRange(0, -1).getCell(0, 0).getAddress()}`);
+	 * }
+	 * ```
 	 */
 	export interface CustomConditionalFormat {
 		/**
